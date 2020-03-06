093f3858ec50e1ee75e662a0ae484d387320877c
pzim-devdata <52496172+pzim-devdata@users.noreply.github.com>
2020-03-06 19:04:26 +0000

    Update VPNautoconnect.sh

--- VPNautoconnect/VPNautoconnect.sh
+++ VPNautoconnect/VPNautoconnect.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 while [ "true" ]
 do
-	VPNCON=$(nmcli con status)
-	if [[ $VPNCON != *fr-uk5.nordvpn.com.tcp* ]]; then
-		echo "Disconnected, trying to reconnect..."
-		(sleep 1s && nmcli con up uuid 31445679-af7c-7884-829e-cfd48edf4f59)
+	VPNNAME=$(nmcli con status)
+	if [[ $VPNNAME != *fr.myvpn.com.tcp* ]]; then
+		echo "You ar disconnected from the VPN !"
+		(sleep 2s && nmcli con up uuid 31445679-af7c-7485-829e-cfd48edf4f59  )
 	else
-		echo "Already connected !"
+		echo "You are connected to the VPN !"
 	fi
 	sleep 30
 done
