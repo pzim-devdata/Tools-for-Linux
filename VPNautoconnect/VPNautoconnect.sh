57824bc5b8589af0d3dd0f54fa6dbe3f8a3f5fce
pzim-devdata <52496172+pzim-devdata@users.noreply.github.com>
2020-03-06 19:27:22 +0000

    Update VPNautoconnect.sh

--- VPNautoconnect/VPNautoconnect.sh
+++ VPNautoconnect/VPNautoconnect.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 while [ "true" ]
 do
-	VPNNAME=$(nmcli con status)
+	VPNNAME=$(nmcli con show --active)
 	if [[ $VPNNAME != *fr.myvpn.com.tcp* ]]; then
-		echo "You ar disconnected from the VPN !"
-		(sleep 2s && nmcli con up uuid 31445679-af7c-7485-829e-cfd48edf4f59  )
+		echo "You are disconnected from the VPN ! HELP !!!!!!!!!!!!!!!!!!!!"
+		(sleep 2s && nmcli con up uuid 31445679-af7c-7485-829e-cfd48edf4f59         )
 	else
-		echo "You are connected to the VPN !"
+		echo "You are connected to the VPN ! All seems good :-)"
 	fi
 	sleep 5
 done
